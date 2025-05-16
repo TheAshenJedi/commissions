e680d4cdc533469016fddd842b76245723a48fd6
TheAshenJedi <brucehoughton94@gmail.com>
2025-05-15 20:35:52 -0400

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,6 +1,18 @@
 var data = {
 	images: [
 		{
+			src: [""],
+			thumbnail: "",
+			title: "Placeholder Entry",
+			artist: "Placeholder Entry",
+			artist_url: null,
+			art_url: null,
+			desc: "Do not delete this. You can use this to set the order the tags show up in the dropdown",
+			date_str: "Beginning of Time",
+			tags: ["ingrid", "sfw"],
+			hidden: true
+		},
+		{
 			src: ["https://cdn.imgchest.com/files/yvdcw8jdxjy.png"],
 			thumbnail: "https://cdn.imgchest.com/files/y8xcn9bknq4.png",
 			title: "Jedi Ingrid (Xander Wolk)",
@@ -8,21 +20,9 @@ var data = {
 			artist_url: "https://bsky.app/profile/xander-wolk.bsky.social",
 			art_url: "https://bsky.app/profile/xander-wolk.bsky.social/post/3ll5speu2fc2x",
 			desc: "Jedi Knight Ingrid Brandl Galatea reporting for duty",
-			date_str: "March 24 2025",
+			date_str: "March 24th 2025",
 			tags: ["ingrid", "sfw"],
-			hidden: true
-		},
-		{
-			src: [""],
-			thumbnail: "",
-			title: "",
-			artist: "",
-			artist_url: "",
-			art_url: "",
-			desc: "",
-			date_str: "",
-			tags: [""],
-			hidden: true
+			hidden: false
 		},
 		{
 			src: [""],

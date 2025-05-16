749ee3cca415fdf4114c47c2eb2357ff73fe175f
TheAshenJedi <brucehoughton94@gmail.com>
2025-05-15 20:48:27 -0400

    Update data.js

--- js/data.js
+++ js/data.js
@@ -9,7 +9,7 @@ var data = {
 			art_url: null,
 			desc: "Do not delete this. You can use this to set the order the tags show up in the dropdown.",
 			date_str: "Beginning of Time",
-			tags: ["ingrid", "sfw"],
+			tags: [],
 			hidden: true
 		},
 		{
